6d8624722097c9c011a2640d84a6d81b9d5c705a
Martin Alzaga 2 <martinalzaga2@MacBook-Air-de-Martin.local>
2022-05-14 11:54:24 -0300

    homework

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -71,31 +71,35 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (str1 = str2){
-    return true;
-  } else {
-    return false;
-  }
+  return str1.lenght === str2.lenght;
 }
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  
-}
+  if (num < 90) {
+  return true;
+  } else {
+    return false;
+  }
+  }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  
-}
+  if (num > 50) {
+    return true;
+  } else {
+    return false;
+  }
+  }
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
-  
+  return x % y;
 }
 
 function esPar(num) {
