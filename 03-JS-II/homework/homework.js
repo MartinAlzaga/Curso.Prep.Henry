fa1b0b5c9948e5799dc3d54e850ebc4117758536
Martin Alzaga 2 <martinalzaga2@MacBook-Air-de-Martin.local>
2022-05-17 17:24:01 -0300

    homework 3

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -47,6 +47,18 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if (idioma === aleman){
+    return "Guten Tag!"
+  }
+  else if (idioma === mandarin){
+    return "Ni Hao!"
+  }
+  else if (idioma === ingles){
+    return "Hello!"
+  }
+  else{
+    return "undefined" | "Hola!"
+  }
 }
 
 function colors(color) {
