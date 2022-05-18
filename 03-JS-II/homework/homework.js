ee9282f2f3f98ca592ff123bf9240ef3cbd9fd81
Martin Alzaga 2 <martinalzaga2@MacBook-Air-de-Martin.local>
2022-05-18 12:07:25 -0300

    Homework 3

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -91,12 +91,30 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero === 10){
+    return "true"
+  }
+  else if (numero === 5){
+    return "true"
+  } 
+  else {
+    return "false"
+  }
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero < 50){
+    return "true"
+  }
+  else if (numero > 20){
+    return "true"
+  }
+  else {
+    return "false"
+  }
 }
 
 function esEntero(numero) {
