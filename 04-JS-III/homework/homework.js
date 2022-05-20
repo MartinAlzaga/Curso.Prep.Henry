045adbe7e47a29dec3251806f40923e5d58f194a
Martin Alzaga 2 <martinalzaga2@MacBook-Air-de-Martin.local>
2022-05-20 18:25:57 -0300

    Homework 4

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -34,7 +34,7 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
-  array.pop("elemento");
+  array.pop(elemento);
     return array
 }
 
@@ -44,7 +44,7 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
-  array.unshift("elemento");
+  array.unshift(elemento);
     return array
 }
 
